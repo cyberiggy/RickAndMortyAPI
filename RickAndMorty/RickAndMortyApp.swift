431a43673442f0a5d41b5015d39c72ab75b9dd17
Ron Woodbury <ronwoodbury@mac.com>
2021-11-18 01:48:47 -0700

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Ron Woodbury on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
